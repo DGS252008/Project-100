cb6c0de5a6b4f5898b5f1bcc4bc010258e3b8ede
TombTumbler <71518638+DGS252008@users.noreply.github.com>
2022-04-19 12:05:57 -0700

    Add files via upload

--- Project-100.py
+++ Project-100.py
@@ -1,14 +1,36 @@
 class Atm(object):
-    bankBalance = 1,000,000
     def __init__(self, card_number, pin_number):
         self.card_number = card_number
-        self.pin_number = pin_number
+        self.pin = pin_number
 
-    def CashWithdrawal():
-        print('Withdrawing cash...' + bankBalance + '-100')
-        bankBalance -= 100
+    def cash_withdrawl(self, amount):
+        new_amount = 1000000 - amount
+        print("You have withdrawn " + str(amount) + " dollars. Your balance is " + str(new_amount))
 
-    def BalanceInquiry():
-        print('Your bank balance is ' + bankBalance)
+    def check_balance(self):
+        print('Your bank balance is 1000000')
+
+def main():
+    Card_number = input("Please enter your card number: ")
+    pin_number = input("Please enter your pin number: ")
+
+    new_user = Atm(Card_number, pin_number)
+
+    print("Choose your activity: ")
+    print("1. Check your balance   2. Withdraw Cash")
+    activity = int(input("Enter activity number: "))
+
+    if(activity == 1):
+        new_user.check_balance()
+        
+    elif(activity == 2):
+        amount = int(input("Please enter the amount you want to withdraw: "))
+        new_user.cash_withdrawl(amount)
+        
+    else:
+        print("Please enter a valid activity number.")
+
+if __name__ =="__main__":
+    main()
 
 
